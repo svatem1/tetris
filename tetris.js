dde77d291ac904d7cd867e541a9e669044cee620
Martin Svatek <svatem1@gmail.com>
2022-08-27 16:49:11 +0200

    Cleaned up repo, moved some js code to css, made some shortcuts.

--- tetris.js
+++ tetris.js
@@ -1,8 +1,13 @@
-// visible grid is 10x20, grid is 10x22, 2 hidden lines for initial piece position
-
 const rgba = (r, g, b, a) => (r | g << 8 | b << 16 | a << 24) >>> 0; // >>> 0 for int/uint conversion
 
+
 const VOID = rgba(35, 35, 35, 255); // background color
+
+// visible grid is 10x20, grid is 10x22, 2 hidden lines for initial piece position
+const GRID = (new Uint32Array(10 * 22)).fill(VOID); // we need the initialization for the 1st run of the game
+
+// DOM is already constructed at this point in time, because of html structure
+const CANVAS = new Map(["grid", "piece"].map(id => [id, document.getElementById(id).getContext("2d")]));
 
 const PIECES = [
 // http://tetris.wikia.com/wiki/Tetromino
@@ -16,9 +21,8 @@ const PIECES = [
     { data: [[[-1, 1], [0, 1], [0, 0], [1, 0]]], rots: 2, color: rgba(41, 173, 255, 255) }, // Z
     { data: [[[-1, 1], [-1, 0], [0, 0], [1, 0]]], rots: 4, color: rgba(255, 119, 168, 255) }, // J
     { data: [[[-1, 0], [0, 0], [1, 0], [1, 1]]], rots: 4, color: rgba(255, 204, 170, 255) }  // L
-];
-
-const grid = (new Uint32Array(10 * 22)).fill(VOID);
+// precalculates look-up table with rotated pieces
+].forEach(piece => [...Array(piece.rots - 1).keys()].forEach(i => piece.data.push(piece.data[i].map(([x, y]) => [y, -x]))));
 
 
 const xy2grid = ([x, y]) => (21 - y) * 10 + x;
@@ -27,87 +31,44 @@ const inside = ([x, y]) => x >= 0 && x <= 9 && y >= 0 && y <= 21;
 
 const vecAdd = (a, b) => a.map((el, i) => el + b[i]);
 
-function drawGrid(where, grid) {
-    canvas.get(where).ctx.putImageData(new ImageData(new Uint8ClampedArray(grid.buffer, 10 * 2 * 4, 10 * 20 * 4), 10, 20), 0, 0);
-}
+const drawGrid = (where, grid) => CANVAS.get(where).putImageData(new ImageData(new Uint8ClampedArray(grid.buffer, 10 * 2 * 4, 10 * 20 * 4), 10, 20), 0, 0);
+drawGrid("grid", GRID); // we need this for 1st run of the game
 
-function canPlace({ piece, rot }, pos) {
-    return piece.data[rot].map(i => vecAdd(pos, i)).every(j => inside(j) && grid[xy2grid(j)] === VOID);
-}
+const canPlace = ({ piece, rot }, pos) => piece.data[rot].map(i => vecAdd(pos, i)).every(j => inside(j) && GRID[xy2grid(j)] === VOID);
 
-function place({ piece, rot }, grid, pos) {
-    return (piece.data[rot].forEach(coord => grid[xy2grid(vecAdd(pos, coord))] = piece.color), grid);
-}
+const place = ({ piece, rot }, grid, pos) => (piece.data[rot].forEach(coord => grid[xy2grid(vecAdd(pos, coord))] = piece.color), grid);
 
-function drawPiece(piece, pos) {
-    drawGrid("piece", place(piece, new Uint32Array(new ImageData(10, 22).data.buffer), pos));
-}
+const drawPiece = (piece, pos) => drawGrid("piece", place(piece, new Uint32Array(new ImageData(10, 22).data.buffer), pos));
 
-function iterLines(grid, fn, init) {
-    return [...Array(22).keys()].map(y => xy2grid([0, 21 - y])).reduce((res, offset) =>
-        grid.subarray(offset, offset + 10).every(color => color !== VOID) ? fn(res, offset) : res, init);
-}
+const iterLines = (grid, fn, init) => [...Array(22).keys()].map(y => xy2grid([0, 21 - y])).reduce((res, offset) => grid.subarray(offset, offset + 10).every(color => color !== VOID) ? fn(res, offset) : res, init);
 
-function shrinkLines(grid) {
-    return iterLines(grid, (r, offset) => grid.copyWithin(10, 0, offset).subarray(0, 10).fill(VOID), grid);
-}
+const shrinkLines = (grid) => iterLines(grid, (_, offset) => grid.copyWithin(10, 0, offset).subarray(0, 10).fill(VOID), grid);
 
-function countLines(grid) {
-    return iterLines(grid, (res,) => res + 1, 0);
-}
+const countLines = grid => iterLines(grid, (res,) => res + 1, 0);
 
-function precalcPieces() {
-    PIECES.forEach(piece => [...Array(piece.rots - 1).keys()].forEach(i =>
-        piece.data.push(piece.data[i].map(([x, y]) => [y, -x]))));
-}
+const getPiece = piece => ({ piece: piece, rot: 0 });
 
-function getPiece(piece) {
-    return {
-        piece: piece,
-        rot: 0
-    }
-}
+// const rotPieceCCW = ({ piece, rot }) => ({ piece: piece, rot: (rot + piece.rots - 1) % piece.rots });
+const rotPieceCW = ({ piece, rot }) => ({ piece: piece, rot: (rot + 1) % piece.rots });
 
-function rotPiece({ piece, rot }) {
-    return {
-        piece: piece,
-        // rot: (rot + piece.rots - 1) % piece.rots // CCW
-        rot: (rot + 1) % piece.rots // CW
-    }
-}
 
-function resize(ar) {
-    const [x, y] = [window.innerWidth, window.innerHeight];
-    const ratio = y / x;
-    const w = ratio > ar ? x : Math.floor(y / ar);
-    const h = ratio > ar ? Math.floor(ar * x) : y;
-    const wrapper = document.getElementById("wrapper");
-    [["width", w], ["height", h]].forEach(([prop, size]) => wrapper.style[prop] = size.toString() + "px");
-}
-
-function newGame() {
-    grid.fill(VOID);
-    drawGrid("grid", grid);
-    gameplay(0);
-}
-
-function gameplay(shrinkedLines) {
-    const dirs = new Map([[37, [-1, 0]], [39, [1, 0]], [40, [0, -1]], [38, [0, 0]]]);
+(function gameplay(shrinkedLines) {
     let [piece, pos] = [getPiece(PIECES[Math.floor(Math.random() * PIECES.length)]), [4, 20]];
 
     const controls = event => {
+        const dirs = new Map([[37, [-1, 0]], [39, [1, 0]], [40, [0, -1]], [38, [0, 0]]]);
         if (dirs.has(event.keyCode)) {
-            const newPiece = event.keyCode === 38 ? rotPiece(piece) : piece;
+            const newPiece = event.keyCode === 38 ? rotPieceCW(piece) : piece;
             const kicks = (newPiece.rot & 1) ? [[0, 0], [0, 1], [0, 2]] : [[0, 0], [-1, 0], [1, 0], [-2, 0], [2, 0]];
             kicks.some(coord => {
                 const newPos = vecAdd(coord, vecAdd(pos, dirs.get(event.keyCode)));
                 if (canPlace(newPiece, newPos)) {
-                    canvas.get("piece").ctx.clearRect(0, 0, 10, 22);
+                    CANVAS.get("piece").clearRect(0, 0, 10, 22);
                     drawPiece(newPiece, newPos);
                     [piece, pos] = [newPiece, newPos];
                     return true;
                 }
-                if (event.keyCode !== 38) return true;
+                return event.keyCode !== 38;
             });
         }
         event.preventDefault();
@@ -115,40 +76,25 @@ function gameplay(shrinkedLines) {
 
     const down = () => {
         const newPos = vecAdd(pos, [0, -1]);
-        canvas.get("piece").ctx.clearRect(0, 0, 10, 22);
+        CANVAS.get("piece").clearRect(0, 0, 10, 22);
         if (canPlace(piece, newPos)) {
             drawPiece(piece, newPos);
             pos = newPos;
             setTimeout(down, shrinkedLines > 100 ? 80 : 80000 / (9 * shrinkedLines + 100));
         } else {
-            const lines = countLines(place(piece, grid, pos));
-            drawGrid("grid", shrinkLines(grid));
+            const lines = countLines(place(piece, GRID, pos));
+            drawGrid("grid", shrinkLines(GRID));
             document.body.onkeydown = undefined;
             setTimeout(() => gameplay(shrinkedLines + lines), 0); // to avoid recursion
         }
     };
 
-    if (!canPlace(piece, pos))
-        newGame();
-    else {
+    if (!canPlace(piece, pos)) {
+        GRID.fill(VOID);
+        drawGrid("grid", GRID);
+        gameplay(0);
+    } else {
         setTimeout(down, 0);
         document.body.onkeydown = controls;
     }
-}
-
-window.onload = function() {
-    canvas = new Map(["grid", "piece"].map((id, i) => { // note: this creates global variable and it's desirable
-        const cnv = document.createElement("canvas");
-        [cnv.width, cnv.height, cnv.zIndex] = [10, 20, i];
-        document.getElementById("wrapper").appendChild(cnv);
-        return [id, {
-            canvas: cnv,
-            ctx: cnv.getContext("2d")
-        }]
-    }));
-
-    precalcPieces();
-    window.onresize = () => resize(2);
-    resize(2);
-    newGame();
-};
+})(0);
